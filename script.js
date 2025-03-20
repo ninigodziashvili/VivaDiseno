936c0ab445e954004f06272f5ce402239a93af63
ninigodziashvili <nino.godziashvili@gmail.com>
2025-03-20 13:36:03 +0100

    Creating nav menu responsive

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-function toggleMenu() {
-    const menu = document.getElementById("menu");
-    menu.classList.toggle("hidden");
-}
+    const navSlide = () => {
+        const burger = document.querySelector(".burger");
+        const nav = document.querySelector(".nav-link");
+        const navLinks = document.querySelectorAll(".nav-link li");
+        
+        burger.addEventListener("click", () => {
+          nav.classList.toggle("nav-link-activated");
+          burger.classList.toggle("toggle")
+          navLinks.forEach((link, index) => {
+            if(link.style.animation){
+              link.style.animation = "";
+            }else{
+              link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`
+            }
+          })
+        });
+      }
+      
+      navSlide();
